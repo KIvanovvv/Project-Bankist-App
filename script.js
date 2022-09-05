f31b08e0d064d451bd70cd724b39f02672b2cf74
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-09-05 19:17:20 +0100

    Adding Loan feature

--- script.js
+++ script.js
@@ -194,6 +194,16 @@ btnTransfer.addEventListener(`click`, function (e) {
     updateUI(currentAccount);
   }
 });
+btnLoan.addEventListener(`click`, function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    //Add movement
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = ``;
+});
 
 btnClose.addEventListener(`click`, function (e) {
   e.preventDefault();
